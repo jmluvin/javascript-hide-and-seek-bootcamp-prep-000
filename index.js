ca9d8673ed82d65a6b4a6be6105386f91131d666
Matthew Luvin <jmluvin@gmail.com>
2019-02-06 23:28:46 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ function deepestChild() {
 function increaseRankBy(n) {
   const lis = document
   .getElementById('app')
-  .querySelectorAll('ul.ranked li');
+  .querySelectorAll('ul.ranked-list li');
   for (let i = 0; i < lis.length; i++) {
     lis[i].innerHTML = (i + 1).tostring();
   }
