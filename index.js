0d5198815546170736d4c573429cfa43fb275470
Matthew Luvin <jmluvin@gmail.com>
2019-02-06 22:29:32 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ function nestedTarget() {
   return document.querySelector('div#nested div.target')
 }
 function deepestChild() {
-  return document.querySelector('div#grand-node div div ')
+  return document.querySelector('div#grand-node div div div div')
 }
 function increaseRankBy(n) {
   
